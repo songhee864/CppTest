2850928b2f5c87daafef30d1f77dd395e62aa0ed
songhee864 <57287524+songhee864@users.noreply.github.com>
2023-09-13 22:40:44 +0900

    Update Test.cpp

--- Test/Test/Test.cpp
+++ Test/Test/Test.cpp
@@ -26,29 +26,28 @@ int main()
     int a, b;
     char c;
 
-    cout << "해당 코드는 입력한 두 수를 사칙연산하는 코드입니다." <<endl;
-    cout << ('\n') << endl;
+    cout << "해당 코드는 입력한 두 수를 사칙연산하는 코드입니다." << endl;
 
     cout << "원하는 정수를 두 개 입력한 후 원하는 사칙연산의 기호를 입력하십시오." << endl;
     cin >> a;
     cin >> b;
-    
+
     cout << "(더하기: +, 빼기: -, 곱하기: *, 나누기: /)" << endl;
     cin >> c;
 
-    switch(c){
-        case '+':
-            cout << sum(a, b);
-            break;
-        case '-':
-            cout << sub(a, b);
-            break;
-        case '*':
-            cout << multi(a, b);
-            break;
-        case '/':
-            cout << divi(a, b);
-            break;
+    switch (c) {
+    case '+':
+        cout << sum(a, b);
+        break;
+    case '-':
+        cout << sub(a, b);
+        break;
+    case '*':
+        cout << multi(a, b);
+        break;
+    case '/':
+        cout << divi(a, b);
+        break;
     }
 
     return 0;
